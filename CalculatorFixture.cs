0da9570bd2aa6ecfa5fdbc7e7de8b4156aa2487c
Mike <45199835+MikeNoelHiggs@users.noreply.github.com>
2022-02-17 00:00:57 +0000

    Initial Commit
    Commited the CS files that were used in the project.

---
+++ CalculatorFixture.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
+
+namespace Calculator_UI_WPF
+{
+    class CalculatorFixture
+    {
+        public int? result;
+        public double? dresult;
+        public string display = "0";
+        public static DataTable dt = new DataTable();
+
+        /// <summary> Adds two values together. </summary>
+        /// <param name="a">Value to have value 'b' added to</param>
+        /// <param name="b">Value to add to value 'a'</param>
+        public static int Add(int a, int b)
+        {
+            int res = a + b;
+            return res;
+        }
+
+        /// <summary> Adds two decimal values together. </summary>
+        /// <param name="a">Value to have value 'b' added to</param>
+        /// <param name="b">Value to add to value 'a'</param>
+        public static double Add(double a, double b)
+        {
+            double res = a + b;
+            return res;
+        }
+
+        /// <summary> Subtracts a value from another. </summary>
+        /// <param name="a">Value for value 'b' to be subtracted from.</param> 
+        /// <param name="b">Value to subtract from value 'a'</param>
+        public static int Subtract(int a, int b)
+        {
+            int res = a - b;
+            return res;
+        }
+
+        /// <summary> Subtracts a decimal value from another. </summary>
+        /// <param name="a">Value for value 'b' to be subtracted from.</param> 
+        /// <param name="b">Value to subtract from value 'a'</param>
+        public static double Subtract(double a, double b)
+        {
+            double res = a - b;
+            return res;
+        }
+
+        /// <summary> Multiplies one value by another </summary>
+        /// <param name="a"> Value to be multiplied by value 'b'. </param> 
+        /// <param name="b"> Value to multiply value 'a' by. </param>
+        public static int Multiply(int a, int b)
+        {
+            int res = a * b;
+            return res;
+        }
+
+        /// <summary> Multiplies one decimal value by another </summary>
+        /// <param name="a"> Value to be multiplied by value 'b'. </param> 
+        /// <param name="b"> Value to multiply value 'a' by. </param>
+        public static double Multiply(double a, double b)
+        {
+            double res = a * b;
+            return res;
+        }
+
+        /// <summary> Divides one value by the another. </summary>
+        /// <param name="a"> Value to divide by value 'b' </param> 
+        /// <param name="b"> Value to divde value 'a' by. </param>
+        public static int? Divide(int a, int b)
+        {
+            try
+            {
+                if (b == 0)
+                {
+                    return null;
+                }
+
+                else
+                {
+                    int res = a / b;
+                    return res;
+                }
+            }
+
+            catch (DivideByZeroException e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                return null;
+            }
+        }
+
+        /// <summary> Divides one Decimal value by the another. </summary>
+        /// <param name="a"> Value to divide by value 'b' </param> 
+        /// <param name="b"> Value to divde value 'a' by. </param>
+        public static double? Divide(double a, double b)
+        {
+            try
+            {
+                if (b == 0)
+                {
+                    return null;
+                }
+
+                else
+                {
+                    double res = a / b;
+                    return res;
+                }
+            }
+
+            catch (DivideByZeroException e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                return null;
+            }
+        }
+
+        /// <summary> Gives remainder after dividing a value by the another. </summary>
+        /// <param name="a"> Value to divide by value 'b' </param> 
+        /// <param name="b"> Value to divde value 'a' by. </param>
+        public static int? Mod(int a, int b)
+        {
+            // Catch divide by zero exception, print the stack trace to the debug console, return null as result.
+            try
+            {
+                if (b == 0)
+                {
+                    return null;
+                }
+
+                else
+                {
+                    int res = a % b;
+                    return res;
+                }
+            }
+
+            catch (DivideByZeroException e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                return null;
+            }
+        }
+
+        /// <summary> Returns square route of a given value </summary>
+        /// <param name="a"> Value to return sqaure route of </param> 
+        public static int? Sqrt(int a)
+        {
+            try
+            {
+                if (a < 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    int res = (Convert.ToInt32(Math.Round((Math.Sqrt(a)))));
+                    return res;
+                }
+            }
+
+            catch (OverflowException e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                return null;
+            }
+
+            catch (FormatException e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                return null;
+            }
+
+        }
+
+        public static double? Sqrt(double a)
+        {
+            try
+            {
+                if (a < 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    double res = (Math.Sqrt(a));
+                    return res;
+                }
+            }
+            catch (OverflowException e)
+            {
+                {
+                    Debug.WriteLine(e.StackTrace);
+                    return null;
+                }
+            }
+        }
+
+        /// <summary> Returns the result of sqauring the value (To it's second power) e.g 2*2 or 2^2 </summary>
+        /// <param name="a"> Value to raise to second power. </param> 
+        public static int? Sqr(int a)
+        {
+            try
+            {
+                if (a < Math.Sqrt(int.MaxValue))
+                {
+                    int? res = Convert.ToInt32(Math.Pow(a, 2));
+                    return res;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (OverflowException e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                return null;
+            }
+
+        }
+
+        /// <summary> Returns the result of sqauring the Decimal value (To it's second power) e.g 2.2*2.2 or 2.2^2.0 </summary>
+        /// <param name="a"> Value to raise to second power. </param> 
+        public static double? Sqr(double a)
+        {
+            try
+            {
+                if (a < Math.Sqrt(double.MaxValue))
+                {
+                    double? res = (Math.Pow(a, 2));
+                    return res;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (OverflowException e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                return null;
+            }
+            catch (FormatException e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                return null;
+            }
+
+        }
+
+        /// <summary> Returns the result of Cubeing the value (To it's Third power) e.g 2*2*2 or 2^3 </summary>
+        /// <param name="a"> Value to raise to Third power. </param> 
+        public static int Cube(int a)
+        {
+            int res = a ^ 3;
+            return res;
+        }
+
+        /// <summary> Returns the result of raising a given value to a given power </summary>
+        /// <param name="x"> Value to be raised by the exponant'</param>
+        /// <param name="exponent"> The exponant to raise the value 'x' to. </param>
+        public static long Raise(long x, long exponent)
+        {
+            long res = x ^ exponent;
+            return res;
+        }
+
+        public string GetDisplay()
+        {
+            return this.display;
+        }
+
+        public void Clear()
+        {
+            display = "0";
+        }
+
+
+        /// <summary> Evaluate a given expression from the Calculator TextBox.</summary>
+        /// <param name="exp">the expression to be evluated</param>
+        /// <returns>The result of the evaluated expression</returns>
+        public static double? EvaluateD(string exp)
+        {
+            try
+            {
+                return Convert.ToDouble(dt.Compute(exp, ""));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.StackTrace);
+                return null;
+            }
+
+        }
+
+
+        /// <summary> Evaluate a given expression from the Calculator TextBox.</summary>
+        /// <param name="exp">the expression to be evluated</param>
+        /// <returns>The result of the evaluated expression</returns>
+        public static int? EvaluateI(string exp)
+        {
+            try
+            {
+                return Convert.ToInt32(dt.Compute(exp, ""));
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                return null;
+            }
+        }
+    }
+}
+
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,431 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator_UI_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public bool svc = false; // Switch control variable
+        public char[] invalidFormat = { '+', '-', '*', '/', '%', '(', ')', 'E' };
+
+        private readonly CalculatorFixture calc = new CalculatorFixture();
+
+        /// <summary>
+        ///  Makes a null return present "ERROR" to the user instead.
+        /// </summary>
+        private void NullToError()
+        {
+            if (calc.result == null && calc.dresult == null)
+            {
+                txtResult.Text = "ERROR";
+            }
+
+            if (calc.dresult == Convert.ToDouble("∞"))
+            {
+                txtResult.Text = "ERROR";
+            }
+
+            if (txtResult.Text == "")
+            {
+                txtResult.Text = "ERROR";
+            }
+
+            if (txtResult.Text == "NaN")
+            {
+                txtResult.Text = "ERROR";
+            }
+        }
+
+        private void ErrorToZero()
+        {
+            if (txtResult.Text == "ERROR")
+            {
+                txtResult.Text = "0";
+                txtHistory.Text = "0";
+            }
+        }
+
+        // Event Handlers
+
+        // Handler for all Number buttons
+        private void BtnNumber_Click(object sender, EventArgs e)
+        {
+            if (sender is Button btn)
+            {
+
+
+                ErrorToZero();
+                switch ((btn.Name))
+                {
+                    case "btnZero":
+                        if (txtResult.Text == "0")
+                        {
+                            txtResult.Text = "0";
+                        }
+                        else
+                        {
+                            txtResult.Text += "0";
+                        }
+                        break;
+
+                    case "btnOne":
+                        if (txtResult.Text == "0")
+                        {
+                            txtResult.Text = "1";
+                        }
+                        else
+                        {
+                            txtResult.Text += "1";
+                        }
+                        break;
+
+                    case "btnTwo":
+                        if (txtResult.Text == "0")
+                        {
+                            txtResult.Text = "2";
+                        }
+                        else
+                        {
+                            txtResult.Text += "2";
+                        }
+                        break;
+
+                    case "btnThree":
+                        if (txtResult.Text == "0")
+                        {
+                            txtResult.Text = "3";
+                        }
+                        else
+                        {
+                            txtResult.Text += "3";
+                        }
+                        break;
+
+                    case "btnFour":
+                        if (txtResult.Text == "0")
+                        {
+                            txtResult.Text = "4";
+                        }
+                        else
+                        {
+                            txtResult.Text += "4";
+                        }
+                        break;
+
+                    case "btnFive":
+                        if (txtResult.Text == "0")
+                        {
+                            txtResult.Text = "5";
+                        }
+                        else
+                        {
+                            txtResult.Text += "5";
+                        }
+                        break;
+
+                    case "btnSix":
+                        if (txtResult.Text == "0")
+                        {
+                            txtResult.Text = "6";
+                        }
+                        else
+                        {
+                            txtResult.Text += "6";
+                        }
+                        break;
+
+                    case "btnSeven":
+                        if (txtResult.Text == "0")
+                        {
+                            txtResult.Text = "7";
+                        }
+                        else
+                        {
+                            txtResult.Text += "7";
+                        }
+                        break;
+
+                    case "btnEight":
+                        if (txtResult.Text == "0")
+                        {
+                            txtResult.Text = "8";
+                        }
+                        else
+                        {
+                            txtResult.Text += "8";
+                        }
+                        break;
+
+                    case "btnNine":
+                        if (txtResult.Text == "0")
+                        {
+                            txtResult.Text = "9";
+                        }
+                        else
+                        {
+                            txtResult.Text += "9";
+                        }
+                        break;
+                }
+            }
+        }
+       
+        private void BtnBackspace_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            if (txtResult.Text == "ERROR")
+            {
+
+            }
+            else
+            {
+                if (txtResult.Text.Length >= 1)
+                {
+                    txtResult.Text = txtResult.Text.Remove(txtResult.Text.Length - 1);
+
+                    if (txtResult.Text == "")
+                    {
+                        txtResult.Text = "0";
+                    }
+                }
+            }
+        }
+
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            calc.Clear();
+            txtResult.Text = calc.GetDisplay();
+        }
+
+        private void BtnClearE_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            calc.Clear();
+            txtHistory.Text = "";
+            txtResult.Text = calc.GetDisplay();
+        }
+
+        private void BtnDecimal_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            if (txtResult.Text != "ERROR")
+            {
+                if (txtResult.Text == "0")
+                {
+                    txtResult.Text = "0.";
+                }
+                else if (txtResult.Text == "-0")
+                {
+                    txtResult.Text = "-0.";
+                }
+                else
+                {
+                    txtResult.Text += ".";
+                }
+            }
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            if (txtResult.Text == "0")
+            {
+                txtResult.Text = " + ";
+            }
+            else
+            {
+                txtResult.Text += " + ";
+            }
+        }
+
+        private void BtnMinus_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            if (txtResult.Text == "0")
+            {
+                txtResult.Text = " - ";
+            }
+            else
+                txtResult.Text += " - ";
+        }
+
+        private void BtnMult_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            txtResult.Text += " * ";
+        }
+
+        private void BtnDiv_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            txtResult.Text += " / ";
+        }
+
+
+
+        private void BtnOpenB_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            if (txtResult.Text == "0")
+            {
+                txtResult.Text = "(";
+                svc = true;
+            }
+            else
+            {
+                txtResult.Text += "(";
+            }
+        }
+
+        private void BtnCloseB_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            if (txtResult.Text == "0")
+            {
+                txtResult.Text = ")";
+                svc = true;
+            }
+            else
+            {
+                txtResult.Text += ")";
+            }
+        }
+
+        private void BtnSqr_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            if (!CheckFormat())
+            {
+                if (!txtResult.Text.Contains('.'))
+                {
+                    calc.result = CalculatorFixture.Sqr(Convert.ToInt32(txtResult.Text));
+                    txtHistory.Text = txtResult.Text + "² = ";
+                    txtResult.Text = Convert.ToString(calc.result);
+                }
+                else if (txtResult.Text.Contains('.'))
+                {
+                    calc.dresult = CalculatorFixture.Sqr(Convert.ToDouble(txtResult.Text));
+                    txtHistory.Text = txtResult.Text + "² = ";
+                    txtResult.Text = Convert.ToString(calc.dresult);
+                }
+                else
+                {
+                    calc.result = null;
+                }
+            }
+            else
+            {
+                calc.result = null;
+            }
+            NullToError();
+        }
+
+        private void BtnSqrt_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            if (CheckFormat())
+            {
+                txtResult.Text = "ERROR";
+            }
+            else
+            {
+                calc.dresult = CalculatorFixture.Sqrt(Convert.ToDouble(txtResult.Text));
+                txtHistory.Text = txtResult.Text + "√ = ";
+                txtResult.Text = Convert.ToString(calc.dresult);
+            }
+        }
+
+        private void BtnSign_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            if (txtResult.Text != "ERROR")
+            {
+                if (txtResult.Text.StartsWith("-"))
+                {
+                    txtResult.Text = txtResult.Text.Remove(0, 1);
+                }
+
+                else if (txtResult.Text == "0")
+                {
+                    txtResult.Text = "-";
+                }
+
+                else
+                {
+                    txtResult.Text = "-" + txtResult.Text;
+                }
+            }
+        }
+
+        private void BtnEquals_Click(object sender, EventArgs e)
+        {
+            ErrorToZero();
+            txtHistory.Text += txtResult.Text + " = ";
+            if (txtResult.Text.Contains('.') || txtResult.Text.Contains('/'))
+            {
+                calc.dresult = CalculatorFixture.EvaluateD(txtResult.Text);
+                txtResult.Text = Convert.ToString(calc.dresult);
+                //txtHistory.Text += Convert.ToString(calc.dresult) + ", ";
+            }
+            else
+            {
+                calc.result = CalculatorFixture.EvaluateI(txtResult.Text);
+                txtResult.Text = Convert.ToString(calc.result);
+                //txtHistory.Text += Convert.ToString(calc.result) + ", ";
+            }
+            NullToError();
+        }
+
+        /// <summary>
+        /// This method is used to check for any characters that will cause a formatException
+        /// </summary>
+        public bool CheckFormat()
+        {
+            bool invalidCharacters = false;
+
+            for (int i = 0; i < invalidFormat.Length; i++)
+            {
+                if (!txtResult.Text.Contains(invalidFormat[i]))
+                {
+                    invalidCharacters = false;
+                }
+                else
+                {
+                    invalidCharacters = true;
+                    break;
+                }
+            }
+
+            if (invalidCharacters == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
